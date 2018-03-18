f37f031fb034bd70579a30a7013cc109a6501fa7
AlexMichal <alex.michal@hotmail.com>
2018-03-17 20:47:29 -0700

    Changed signup to have the proper db vars

--- signup.php
+++ signup.php
@@ -6,14 +6,15 @@
 <section class="main_container">
     <div class="main_wrapper">
         <h2>Sign Up</h2>
-        <form class="signup_form">
-            <input type="text" name="user_first_name" placeholder="Firstname">
-            <input type="text" name="user_last_name" placeholder="Lastname">
-            <input type="text" name="user_email" placeholder="E-mail">
-            <input type="text" name="user_username" placeholder="Username">
-            <input type="password" name="user_password" placeholder="Password">
-            <button type="submit" name="submit">Sign Up</button>        
-        </div>
+        <form class="signup_form" action="includes/signup.inc.php" method="POST">
+            <input type="text" name="first_name" placeholder="Firstname">
+            <input type="text" name="last_name" placeholder="Lastname">
+            <input type="text" name="email" placeholder="E-mail">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="password" placeholder="Password">
+            <button type="submit" name="submit">Sign Up</button>
+        </form>
+    </div>
 </section>
 
 <?php
