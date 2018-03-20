791479114d0a5779e8c1320f24c3e701e5d87e6e
AlexMichal <alex.michal@hotmail.com>
2018-03-19 19:56:09 -0700

    Fixed credentials for connecting to database

--- public_html/includes/dbh.inc.php
+++ public_html/includes/dbh.inc.php
@@ -1,17 +1,20 @@
 <?php
 
-$deployed = false;
+$deployed = true;
 
 if ($deployed) {
-    $dbServername = "mysql://az7ops07y8zb0lzz:cvxrk814tkr5s2x2@qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/o5b26p64rbcl8s4x";
-    $dbusername = "az7ops07y8zb0lzz";
+    $dbServerName = "qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dbUsername = "az7ops07y8zb0lzz";
     $dbPassword = "cvxrk814tkr5s2x2";
-    $dbName = "loginsystemphp";
+    $dbName = "o5b26p64rbcl8s4x";
+    $dbPort = "3306";
+
+    $conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName, dbPort);
 } else {
-    $dbServername = "localhost";
-    $dbusername = "root";
+    $dbServerName = "localhost";
+    $dbUsername = "root";
     $dbPassword = "";
     $dbName = "loginsystem";
-}
 
-$conn = mysqli_connect($dbServername, $dbusername, $dbPassword, $dbName);
+    $conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
+}
