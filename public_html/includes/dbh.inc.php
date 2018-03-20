76d6dac41da5891c22a50c4dd2dd47488ee371b5
AlexMichal <alex.michal@hotmail.com>
2018-03-19 19:34:14 -0700

    Fixed error in dbh.inc.php

--- public_html/includes/dbh.inc.php
+++ public_html/includes/dbh.inc.php
@@ -2,7 +2,7 @@
 
 $deployed = true;
 
-if (deployed) {
+if ($deployed) {
     $dbServername = "mysql://az7ops07y8zb0lzz:cvxrk814tkr5s2x2@qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/o5b26p64rbcl8s4x";
     $dbusername = "az7ops07y8zb0lzz";
     $dbPassword = "cvxrk814tkr5s2x2";
