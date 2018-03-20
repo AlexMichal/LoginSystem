c0fa48d06fd95697e239fd8fe750546b19f542bd
AlexMichal <alex.michal@hotmail.com>
2018-03-19 18:45:02 -0700

    Added connection to deployed database

--- public_html/includes/dbh.inc.php
+++ public_html/includes/dbh.inc.php
@@ -1,8 +1,17 @@
 <?php
 
-$dbServername = "localhost";
-$dbusername = "root";
-$dbPassword = "";
-$dbName = "loginsystem";
+$deployed = true;
+
+if (deployed) {
+    $dbServername = "mysql://az7ops07y8zb0lzz:cvxrk814tkr5s2x2@qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/o5b26p64rbcl8s4x";
+    $dbusername = "az7ops07y8zb0lzz";
+    $dbPassword = "cvxrk814tkr5s2x2";
+    $dbName = "loginsystemphp";
+} else {
+    $dbServername = "localhost";
+    $dbusername = "root";
+    $dbPassword = "";
+    $dbName = "loginsystem";
+}
 
 $conn = mysqli_connect($dbServername, $dbusername, $dbPassword, $dbName);
