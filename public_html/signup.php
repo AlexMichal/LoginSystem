69f078cba40b2cfc0622763d24ff7576efe8c179
Alex Michal <you@example.com>
2018-03-27 00:43:32 -0700

    Updated users.php with getters and setters

--- public_html/signup.php
+++ public_html/signup.php
@@ -5,7 +5,8 @@
 <section class="main_container">
     <div class="main_wrapper">
         <h2>Sign Up</h2>
-        
+
+        <!-- TODO use an object -->
         <form class="signup_form" action="includes/signup.inc.php" method="POST">
             <input type="text" name="first_name" placeholder="First name" class="input">
             <input type="text" name="last_name" placeholder="Last name" class="input">
--- public_html/user.php
+++ public_html/user.php
@@ -3,15 +3,49 @@
 class User {
     private $username;
     private $password;
-    private $first_name;
-    private $last_name;
+    private $firstName;
+    private $lastName;
     private $email;
 
-    function __construct($username, $password, $first_name, $last_name, $email) {
+    function __construct($username, $password, $firstName, $lastName, $email) {
         $this->username = $username;
         $this->password = $password;
-        $this->first_name = $first_name;
-        $this->last_name = $last_name;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+    }
+
+    function __destruct() {
+        // TODO 
+    }
+
+    // GETTERS
+    function getUsername() {
+        return $this->username;
+    }
+
+    function getFirstName() {
+        return $this->firstName;
+    }
+
+    function getLastName() {
+        return $this->lastName;
+    }
+
+    function getEmail() {
+        return $this->email;
+    }
+    
+    // SETTERS
+    function setFirstName($firstName) {
+        $this->firstName = $firstName;
+    }
+
+    function setLastName($lastName) {
+        $this->lastName = $lastName;
+    }
+
+    function setEmail($email) {
         $this->email = $email;
     }
 }
