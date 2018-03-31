dfeab5a5d8b848f82dbc94790a979eb7dbf24e69
AlexMichal <alex.michal@hotmail.com>
2018-03-30 22:12:44 -0700

    Changed db connection to deployed

--- public_html/signup.php
+++ public_html/signup.php
@@ -12,17 +12,17 @@
 
         <!-- TODO use an object -->
         <form id="signup_form" class="form-group" action="includes/signup.inc.php" method="POST">
-            <input type="text" name="first_name" placeholder="First name" class="input form-control">
-            <input type="text" name="last_name" placeholder="Last name" class="input form-control">
-            <input type="text" name="email" placeholder="E-mail" class="input form-control">
-            <input type="text" name="username" placeholder="Username" class="input form-control">
-            <input type="password" name="password" placeholder="Password" class="input form-control">
+            <input type="text" name="first_name" placeholder="first name" class="input form-control">
+            <input type="text" name="last_name" placeholder="last name" class="input form-control">
+            <input type="text" name="email" placeholder="e-mail" class="input form-control">
+            <input type="text" name="username" placeholder="username" class="input form-control">
+            <input type="password" name="password" placeholder="password" class="input form-control">
             <small id="signup_error" class="form-text text-alert"></small>
             <button type="submit" name="submit" class="btn btn-primary btn-lg">Sign Up</button>
         </form>
         
         <div id="signup_login_main" class="main_wrapper">
-            Have an account already? Log in.
+            Have an account already? <a href="">Log in</a>.
         </div>
     </div>
 </section>
