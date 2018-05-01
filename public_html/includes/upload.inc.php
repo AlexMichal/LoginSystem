20a00db9efc2e279d0dceadfda71c5cdd8cb30c8
AlexMichal <alex.michal@hotmail.com>
2018-04-30 19:10:52 -0700

    Cleaned up code

--- public_html/includes/upload.inc.php
+++ public_html/includes/upload.inc.php
@@ -1,8 +1,6 @@
 <?php
 session_start(); // resume current session
 
-define('VALID_FILE_TYPES', array('jpg', 'jpeg', 'png', 'pdf'));
-
 if (isset($_POST['submit'])) { // name we're checking for inside the method is 'submit'
     $file = $_FILES['file']; // FILES is a super global where we get our input from a form where the name is 'file'
     $fileName = $file['name'];
@@ -15,29 +13,35 @@ if (isset($_POST['submit'])) { // name we're checking for inside the method is '
     $allowed = array('jpg', 'jpeg', 'png');
     $validFileSize = "2000000";
    
-    // if (in_array(VALID_FILE_TYPES)) {
     if (in_array($fileActualExtension, $allowed)) {
         if ($fileError === 0) { // 0 means no error
             if ($fileSize < $validFileSize) { 
-                $fileDirectory = "../uploads/" . $_SESSION['u_username'];
-                $fileNameNew = uniqid('', true) . "." . $fileActualExtension; // create a unique number in microseconds and then append the file extension to it77
-                $fileDestination = $fileDirectory . "/" . $fileNameNew;
-                
-                if (!file_exists($fileDirectory)) {
-                    mkdir($fileDirectory, 0777, true);
-                }
+                uploadFile();
 
-                move_uploaded_file($fileTmpFileLocation, $fileDestination);
-                header("Location: ../index.php?uploadSuccess"); // bring back to index.php when we're done
+                header("Location: ../index.php?upload=success");
             } else {
-                echo "Your file is too big.";
+                // Error: filesize too large
+                header("Location: ../uploadimage.php?upload=filesize_too_large");
             }
         } else {
-            print_r($file);
-            echo "</br>There was an error uploading your file!";
+            // Error: file upload error
+            header("Location: ../uploadimage.php?upload=error");
         }
     } else { 
-        echo "You cannot upload files of this type.";
+        // Error: incorrect file type
+        header("Location: ../uploadimage.php?upload=wrong_file_type");
+    }
+
+    function uploadFile() {
+        $fileDirectory = "../uploads/" . $_SESSION['u_username'];
+        $fileNameNew = uniqid('', true) . "." . $fileActualExtension; // create a unique number in microseconds and then append the file extension to it77
+        $fileDestination = $fileDirectory . "/" . $fileNameNew;
+        
+        if (!file_exists($fileDirectory)) {
+            mkdir($fileDirectory, 0777, true);
+        }
+
+        move_uploaded_file($fileTmpFileLocation, $fileDestination);
     }
 }
 ?>
