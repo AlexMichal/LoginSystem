514dc597e89043890d53cb4ca66b4539bb0e9835
AlexMichal <alex.michal@hotmail.com>
2018-05-07 16:20:33 -0700

    Added required statements to the input

--- public_html/signup.php
+++ public_html/signup.php
@@ -10,13 +10,12 @@
             <p>It's easy.</p>
         </div>
 
-        <!-- TODO use an object -->
         <form id="signup_form" class="form-group" action="includes/signup.inc.php" method="POST">
-            <input type="text" name="first_name" placeholder="first name" class="input form-control">
-            <input type="text" name="last_name" placeholder="last name" class="input form-control">
-            <input type="text" name="email" placeholder="e-mail" class="input form-control">
-            <input type="text" name="username" placeholder="username" class="input form-control">
-            <input type="password" name="password" placeholder="password" class="input form-control">
+            <input type="text" name="first_name" placeholder="first name" class="input form-control" required>
+            <input type="text" name="last_name" placeholder="last name" class="input form-control" required>
+            <input type="text" name="email" placeholder="e-mail" class="input form-control" required>
+            <input type="text" name="username" placeholder="username" class="input form-control" required>
+            <input type="password" name="password" placeholder="password" class="input form-control" required>
             <small id="signup_error" class="form-text text-alert"></small>
             <button type="submit" name="submit" class="btn btn-primary btn-lg">Sign Up</button>
         </form>
