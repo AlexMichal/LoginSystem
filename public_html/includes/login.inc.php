fe05b0bf7920c02f0bdd598ad0ec06e8dc3f31bb
AlexMichal <alex.michal@hotmail.com>
2018-05-08 15:01:42 -0700

    Cleaned up the file a bit

--- public_html/includes/login.inc.php
+++ public_html/includes/login.inc.php
@@ -1,55 +1,52 @@
 <?php
 
-session_start(); // starts session inside the website
+session_start(); // Starts session inside the website
 
 if (isset($_POST['submit'])) {
     include 'dbh.inc.php'; 
-
+    
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
+    
+    loginUser($username, $password);
+} else {
+    header("Location: ../index.php?login=error");
+}
 
-    // Error handles
-    // Check if inputs are empty
-    if (empty($username) || empty($password)) {
-        header("Location: ../index.php?login=empty");
-        exit();
-    } else {
-        $sql =  "SELECT * FROM users WHERE user_username = '$username' OR user_email='$username'";
+function loginUser($username, $password) {
+    include 'dbh.inc.php'; 
+
+    // Check for errors: Starting with if inputs are empty
+    if (!empty($username) && !empty($password)) {
+        $sql =  "SELECT * FROM users WHERE user_username = '$username'";
         $result = mysqli_query($conn, $sql);
-        $resultCheck = mysqli_num_rows($result); // returns how many rows found in the db using these params
+        $resultCheck = mysqli_num_rows($result); // Returns how many rows found in the db using these params
         
-        if ($resultCheck < 1) { // no results found, we exit
+        if ($resultCheck < 1) { // No results found, we exit
             header("Location: ../index.php?login=error");
             exit();
         } else {
             if ($row = mysqli_fetch_assoc($result)) {
-                // de-hashing password
+                // De-hashing password
                 $hashedPasswordCheck = password_verify($password, $row['user_password']); // matching up password with the db password
                 
-                // check if passwords are equal
+                // Check if passwords are equal
                 if ($hashedPasswordCheck == false) {
-                    // error if false
+                    // Error if false
                     header("Location: ../index.php?login=invalidpassword");
-                    exit();
                 } elseif ($hashedPasswordCheck == true) {
-                    // true. log in user here.
+                    // True, so log in user here.
                     $_SESSION['u_id'] = $row['user_id'];
                     $_SESSION['u_first_name'] = $row['user_first_name'];
                     $_SESSION['u_last_name'] = $row['user_last_name'];
                     $_SESSION['u_email'] = $row['user_email'];
                     $_SESSION['u_username'] = $row['user_username'];
                 
-                    // send user back to front page with a login success
                     header("Location: ../index.php?login=success");
-                    exit();
                 }
-                
-                //echo $row['user_username']; // example to echo out username
             }
         }
+    } else {
+        header("Location: ../index.php?login=empty");
     }
-} else {
-    // Take back to index page if error
-    header("Location: ../index.php?login=error");
-    exit();
 }
