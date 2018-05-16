2263e2a461ca23ed948bcf5e30849dd1f0721a4a
AlexMichal <alex.michal@hotmail.com>
2018-05-15 18:40:46 -0700

    Reverted back to old build

--- public_html/includes/signup.inc.php
+++ public_html/includes/signup.inc.php
@@ -1,112 +1,80 @@
 <?php 
-
-// require_once '../user.php';
-include_once 'dbh.inc.php';
-
-session_start(); // Starts session inside the website
-
 if (isset($_POST['submit'])) {
+    // TODO once SUCCESS we should change to INDEX
+    include_once 'dbh.inc.php';
+
+    $first = mysqli_real_escape_string($conn, $_POST['first_name']);
+    $last = mysqli_real_escape_string($conn, $_POST['last_name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
-    $firstName = mysqli_real_escape_string($conn, $_POST['first_name']);
-    $lastName = mysqli_real_escape_string($conn, $_POST['last_name']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
 
-    if (empty($username) || empty($password) || empty($firstName) || empty($lastName) || empty($email)) {
-        header("Location: ../signup.php?signup=empty_inputs");
-    } else {
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        $newUser = new User($username, $password, $firstName, $lastName, $email);
+    // Error handlers
+    if (empty($first) || 
+        empty($last) || 
+        empty($email) || 
+        empty($username) || 
+        empty($password)) { // php function to check if inputs are empty
+        // check for errors first
+        header("Location: ../signup.php?signup=empty");
+        exit();
+    }  else {
+        // check if input chars are valid
+        if (!preg_match("/^[a-zA-Z]*$/", $first) ||
+            !preg_match("/^[a-zA-Z]*$/", $last)) { 
+            // php func that goes in to see if we have certain chars in a string
+            header("Location: ../signup.php?signup=invalid");
+            exit();
+        } else { // if we have valid chars
+            // check if email is valid
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                // check for errors in email
+                header("Location: ../signup.php?signup=email");
+                exit();
+            } else {
+                $sql = "SELECT * FROM users WHERE user_username = '$username'";
+                $result = mysqli_query($conn, $sql);
+                $resultCheck = mysqli_num_rows($result);
 
-        //signupUser($newUser);
+                if ($resultCheck > 0) { // we already have a user in the signup page
+                    header("Location: ../signup.php?signup=usertaken");
+                    exit();
+                } else {
+                    // hashing the password
+                    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+                    // insert the user into the database
+                    $sql =  "INSERT INTO users (user_first_name, user_last_name, user_email, user_username, user_password) " .
+                            "VALUES ('$first', '$last', '$email', '$username', '$hashedPassword');";
+                    
+                    // insert data into the database
+                    mysqli_query($conn, $sql);
+                    
+                    // go into the db and select user we just created so that we can use this information in our image tbl
+                    $sql = "SELECT * FROM users WHERE user_username = '$username' AND user_first_name = '$first'";
+                    $results = mysqli_query($conn, $sql); // run the query
+                    
+                    if (mysqli_num_rows($results) > 0 ) {
+                        while($row = mysqli_fetch_assoc($result)) {
+                            $id = $row['user_id'];
+
+                            // set status in profileimage for new user to 1
+                            $sql =  "INSERT INTO profileimage ('$id', 1) " .
+                                    "VALUES ('$first', '$last', '$email', '$username', '$hashedPassword');";
+
+                            // successful signup
+                            header("Location: ../index.php?signup=success");
+                            exit();
+                        }
+                    } else {
+                        header("Location: ../signup.php?signup=usernotfound");
+                    }
+                }
+            }   
+        }
     }
+
 } else {
     header("Location: ../signup.php?signup=failed");
-}
-
-function signupUser($user) {
-    include 'dbh.inc.php';
-
-    // Check if input chars are valid
-    if (containsOnlyLetters($user->getFirstName()) && containsOnlyLetters($user->getLastName())) {
-        // Check if e-mail is valid
-        if (filter_var($user->getEmail(), FILTER_VALIDATE_EMAIL)) {
-            $result = $user->getUserFromDB($conn);
-            $resultCheck = mysqli_num_rows($result);
-
-            if ($resultCheck === 0) {
-                // Add user to DB
-                $user->saveUserToDB($conn);
-                
-                // Get user from DB and use this information in our image table
-                //if ($user->checkIfUserExistsInDB($conn)) {
-                    // $result = $user->getUserFromDB($conn);
-                //    while ($row = mysqli_fetch_assoc($result)) {
-                //     $id = $row['user_id'];
-                //    }
-                    // Set status in profileimage for new user to 0 (0 = no image uploaded)
-                    //$sql =  "INSERT INTO profileimage (user_id, status, filename) " .
-                    //        "VALUES ('$id', 0, '');";
-                    
-                    //mysqli_query($conn, $sql);
-                    header("Location: ../signup.php?signup=complete");
-                    // Finally, log the user in
-                   // logTheUserIntoTheWebsite($user, $conn);        
-                //} else {
-                //    header("Location: ../signup.php?signup=user_not_found");
-                //}
-            } else {
-                // User already exists in the DB
-                header("Location: ../signup.php?signup=user_taken");
-            }
-        } else {
-            // E-mail doesn't follow "xxx@xxx.com" format
-            header("Location: ../signup.php?signup=email");
-        } 
-    } else { // First and Last names are invalid
-        header("Location: ../signup.php?signup=invalid");
-    }
-}
-
-function containsOnlyLetters($value) {
-    // preg_match: value contains certain characters in a string
-    if (preg_match("/^[a-zA-Z]*$/", $value)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function logTheUserIntoTheWebsite($user, $conn) {
-    // Check for errors: Starting with if inputs are empty
-    if (!empty($user->getUsername()) && !empty($user->getPassword())) {
-        $result = $user->getUserFromDB($conn);
-        $resultCheck = mysqli_num_rows($result); // Returns how many rows found in the db using these params
-        
-        if ($resultCheck > 0) {
-            if ($row = mysqli_fetch_assoc($result)) {
-                // De-hashing password and matching up the password with the db password
-                $hashedPasswordCheck = ($user->getPassword() === $row['user_password']);
-
-                if ($hashedPasswordCheck == true) {
-                    $_SESSION['u_id'] = $row['user_id'];
-                    $_SESSION['u_first_name'] = $row['user_first_name'];
-                    $_SESSION['u_last_name'] = $row['user_last_name'];
-                    $_SESSION['u_email'] = $row['user_email'];
-                    $_SESSION['u_username'] = $row['user_username'];
-
-                    // Successful signup
-                    header("Location: ../index.php?signup=success");
-                } elseif ($hashedPasswordCheck == false) {
-                    header("Location: ../index.php?login=invalid_password");
-                } else {
-                    header("Location: ../index.php?login=error");
-                }
-            }
-        } else {
-            header("Location: ../index.php?login=error");
-        }
-    } else {
-        header("Location: ../index.php?login=empty");
-    }
+    exit(); // closes off and stops the script from running
 }
